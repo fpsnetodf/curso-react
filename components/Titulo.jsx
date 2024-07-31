8298d1a036ed0a84cf37053b3b900a60c1434394
fps_netodf <fps_netodf@hotmail.com>
2024-07-31 08:18:08 -0300

    curso de react

--- components/Titulo.jsx
+++ components/Titulo.jsx
@@ -1,9 +1,9 @@
-export const Titulo = () => {
-    const message = "Curso de React"
+export const Titulo = ({msg}) => {
+    
 return (
     
     <div className='bg-sky-300 py-4'>
-        <h1 className="text-4xl  text-center text-green-700 font-bold">{message}</h1>
+        <h1 className="text-4xl text-center text-green-700 font-bold">{msg}</h1>
     </div>
    
 )
--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="flex w-full h-full justify-center">
-      <div className='w-[500px] items-center'>
-       <Titulo />
+    <div className="">
+      <div className=''>
+       <Titulo msg={"Curso de react"} />
     </div>
     </div>
   )
 }
-
 export default App
